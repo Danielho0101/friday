e658e9bf03f4894e3a290ecfb2e6e891aa51fa10
Daniel <lsps08@yahoo.com.hk>
2024-05-21 23:48:44 +0800

    finish JS

--- script.js
+++ script.js
@@ -1,1 +1,535 @@
-console.log("text");
+let btn1a = document.getElementById('btn1a');
+let btnOrange1a = btn1a.style.background = "orange";
+let i1a = 0;
+
+btn1a.addEventListener("click", function onClick(){
+    i1a++;
+    if ( i1a%2 == 0){
+    btn1a.style.background = "orange";
+    btn1a.style.color = "white";
+    btn1a.style.borderColor = "red";
+    btn1a.classList.remove('scale-up-center');
+
+} else if( i1a%2 == !0){
+    btn1a.style.background = "rgb(230, 204, 255)";
+    btn1a.style.color = "purple";
+    btn1a.style.borderColor = "rgb(145, 107, 242)";
+    btn1a.classList.add('scale-up-center');
+}
+
+});
+
+const btn2a = document.getElementById('btn2a');
+const btnOrange2a = btn2a.style.background = "orange";
+let i2a = 0;
+
+btn2a.addEventListener("click", function onClick(){
+    i2a++;
+    if ( i2a%2 == 0){
+    btn2a.style.background = "orange";
+    btn2a.style.color = "white";
+    btn2a.style.borderColor = "red";
+    btn2a.classList.remove('scale-up-center');
+} else if( i2a%2 == !0){
+    btn2a.style.background = "rgb(230, 204, 255)";
+    btn2a.style.color = "purple";
+    btn2a.style.borderColor = "rgb(145, 107, 242)";
+    btn2a.classList.add('scale-up-center');
+}
+
+});
+const btn3a = document.getElementById('btn3a');
+const btnOrange3a = btn3a.style.background = "orange";
+let i3a = 0;
+
+btn3a.addEventListener("click", function onClick(){
+    i3a++;
+    if ( i3a%2 == 0){
+    btn3a.style.background = "orange";
+    btn3a.style.color = "white";
+    btn3a.style.borderColor = "red";
+    btn3a.classList.remove('scale-up-center');
+
+} else if( i3a%2 == !0){
+    btn3a.style.background = "rgb(230, 204, 255)";
+    btn3a.style.color = "purple";
+    btn3a.style.borderColor = "rgb(145, 107, 242)";
+    btn3a.classList.add('scale-up-center');
+}
+});
+
+const btn4a = document.getElementById('btn4a');
+const btnOrange4a = btn4a.style.background = "orange";
+let i4a = 0;
+
+btn4a.addEventListener("click", function onClick(){
+    i4a++;
+    if ( i4a%2 == 0){
+    btn4a.style.background = "orange";
+    btn4a.style.color = "white";
+    btn4a.style.borderColor = "red";
+    btn4a.classList.remove('scale-up-center');
+
+} else if( i4a%2 == !0){
+    btn4a.style.background = "rgb(230, 204, 255)";
+    btn4a.style.color = "purple";
+    btn4a.style.borderColor = "rgb(145, 107, 242)";
+    btn4a.classList.add('scale-up-center');  
+}
+});
+
+// Class B
+const btn1b = document.getElementById('btn1b');
+const btnOrange1b = btn1b.style.background = "orange";
+let i1b = 0;
+
+btn1b.addEventListener("click", function onClick(){
+    i1b++;
+    if ( i1b%2 == 0){
+    btn1b.style.background = "orange";
+    btn1b.style.color = "white";
+    btn1b.style.borderColor = "red";
+    btn1b.classList.remove('scale-up-center');
+} else if( i1b%2 == !0){
+    btn1b.style.background = "rgb(230, 204, 255)";
+    btn1b.style.color = "purple";
+    btn1b.style.borderColor = "rgb(145, 107, 242)";
+    btn1b.classList.add('scale-up-center');
+}
+});
+
+const btn2b = document.getElementById('btn2b');
+const btnOrange2b = btn2b.style.background = "orange";
+let i2b = 0;
+
+btn2b.addEventListener("click", function onClick(){
+    i2b++;
+    if ( i2b%2 == 0){
+    btn2b.style.background = "orange";
+    btn2b.style.color = "white";
+    btn2b.style.borderColor = "red";
+    btn2b.classList.remove('scale-up-center');
+} else if( i2b%2 == !0){
+    btn2b.style.background = "rgb(230, 204, 255)";
+    btn2b.style.color = "purple";
+    btn2b.style.borderColor = "rgb(145, 107, 242)";
+    btn2b.classList.add('scale-up-center');
+}
+});
+
+const btn3b = document.getElementById('btn3b');
+const btnOrange3b = btn3b.style.background = "orange";
+let i3b = 0;
+
+btn3b.addEventListener("click", function onClick(){
+    i3b++;
+    if ( i3b%2 == 0){
+    btn3b.style.background = "orange";
+    btn3b.style.color = "white";
+    btn3b.style.borderColor = "red";
+    btn3b.classList.remove('scale-up-center');
+} else if( i3b%2 == !0){
+    btn3b.style.background = "rgb(230, 204, 255)";
+    btn3b.style.color = "purple";
+    btn3b.style.borderColor = "rgb(145, 107, 242)";
+    btn3b.classList.add('scale-up-center');
+}
+});
+
+const btn4b = document.getElementById('btn4b');
+const btnOrange4b = btn4b.style.background = "orange";
+let i4b = 0;
+
+btn4b.addEventListener("click", function onClick(){
+    i4b++;
+    if ( i4b%2 == 0){
+    btn4b.style.background = "orange";
+    btn4b.style.color = "white";
+    btn4b.style.borderColor = "red";
+    btn4b.classList.remove('scale-up-center');
+
+} else if( i4b%2 == !0){
+    btn4b.style.background = "rgb(230, 204, 255)";
+    btn4b.style.color = "purple";
+    btn4b.style.borderColor = "rgb(145, 107, 242)";
+    btn4b.classList.add('scale-up-center');
+}
+});
+
+// Class C
+const btn1c = document.getElementById('btn1c');
+const btnOrange1c = btn1c.style.background = "orange";
+let i1c = 0;
+
+btn1c.addEventListener("click", function onClick(){
+    i1c++;
+    if ( i1c%2 == 0){
+    btn1c.style.background = "orange";
+    btn1c.style.color = "white";
+    btn1c.style.borderColor = "red";
+    btn1c.classList.remove('scale-up-center');
+} else if( i1c%2 == !0){
+    btn1c.style.background = "rgb(230, 204, 255)";
+    btn1c.style.color = "purple";
+    btn1c.style.borderColor = "rgb(145, 107, 242)";
+    btn1c.classList.add('scale-up-center');
+}
+
+});
+
+const btn2c = document.getElementById('btn2c');
+const btnOrange2c = btn2c.style.background = "orange";
+let i2c = 0;
+
+btn2c.addEventListener("click", function onClick(){
+    i2c++;
+    if ( i2c%2 == 0){
+    btn2c.style.background = "orange";
+    btn2c.style.color = "white";
+    btn2c.style.borderColor = "red";
+    btn2c.classList.remove('scale-up-center');
+
+} else if( i2c%2 == !0){
+    btn2c.style.background = "rgb(230, 204, 255)";
+    btn2c.style.color = "purple";
+    btn2c.style.borderColor = "rgb(145, 107, 242)";
+    btn2c.classList.add('scale-up-center');
+}
+});
+
+const btn3c = document.getElementById('btn3c');
+const btnOrange3c = btn3c.style.background = "orange";
+let i3c = 0;
+
+btn3c.addEventListener("click", function onClick(){
+    i3c++;
+    if ( i3c%2 == 0){
+    btn3c.style.background = "orange";
+    btn3c.style.color = "white";
+    btn3c.style.borderColor = "red";
+    btn3c.classList.remove('scale-up-center');
+
+} else if( i3c%2 == !0){
+    btn3c.style.background = "rgb(230, 204, 255)";
+    btn3c.style.color = "purple";
+    btn3c.style.borderColor = "rgb(145, 107, 242)";
+    btn3c.classList.add('scale-up-center');
+}
+});
+
+const btn4c = document.getElementById('btn4c');
+const btnOrange4c = btn4c.style.background = "orange";
+let i4c = 0;
+
+btn4c.addEventListener("click", function onClick(){
+    i4c++;
+    if ( i4c%2 == 0){
+    btn4c.style.background = "orange";
+    btn4c.style.color = "white";
+    btn4c.style.borderColor = "red";
+    btn4c.classList.remove('scale-up-center');
+} else if( i4c%2 == !0){
+    btn4c.style.background = "rgb(230, 204, 255)";
+    btn4c.style.color = "purple";
+    btn4c.style.borderColor = "rgb(145, 107, 242)";
+    btn4c.classList.add('scale-up-center');
+}
+});
+
+
+// Class D
+const btn1d = document.getElementById('btn1d');
+const btnOrange1d = btn1d.style.background = "orange";
+let i1d = 0;
+
+btn1d.addEventListener("click", function onClick(){
+    i1d++;
+    if ( i1d%2 == 0){
+    btn1d.style.background = "orange";
+    btn1d.style.color = "white";
+    btn1d.style.borderColor = "red";
+    btn1d.classList.remove('scale-up-center');
+} else if( i1d%2 == !0){
+    btn1d.style.background = "rgb(230, 204, 255)";
+    btn1d.style.color = "purple";
+    btn1d.style.borderColor = "rgb(145, 107, 242)";
+    btn1d.classList.add('scale-up-center');
+}
+
+});
+
+const btn2d = document.getElementById('btn2d');
+const btnOrange2d = btn2d.style.background = "orange";
+let i2d = 0;
+
+btn2d.addEventListener("click", function onClick(){
+    i2d++;
+    if ( i2d%2 == 0){
+    btn2d.style.background = "orange";
+    btn2d.style.color = "white";
+    btn2d.style.borderColor = "red";
+    btn2d.classList.remove('scale-up-center');
+} else if( i2d%2 == !0){
+    btn2d.style.background = "rgb(230, 204, 255)";
+    btn2d.style.color = "purple";
+    btn2d.style.borderColor = "rgb(145, 107, 242)";
+    btn2d.classList.add('scale-up-center');
+}
+});
+
+const btn3d = document.getElementById('btn3d');
+const btnOrange3d = btn3d.style.background = "orange";
+let i3d = 0;
+
+btn3d.addEventListener("click", function onClick(){
+    i3d++;
+    if ( i3d%2 == 0){
+    btn3d.style.background = "orange";
+    btn3d.style.color = "white";
+    btn3d.style.borderColor = "red";
+    btn3d.classList.remove('scale-up-center');
+} else if( i3d%2 == !0){
+    btn3d.style.background = "rgb(230, 204, 255)";
+    btn3d.style.color = "purple";
+    btn3d.style.borderColor = "rgb(145, 107, 242)";
+    btn3d.classList.add('scale-up-center');
+}
+});
+
+const btn4d = document.getElementById('btn4d');
+const btnOrange4d = btn4d.style.background = "orange";
+let i4d = 0;
+
+btn4d.addEventListener("click", function onClick(){
+    i4d++;
+    if ( i4d%2 == 0){
+    btn4d.style.background = "orange";
+    btn4d.style.color = "white";
+    btn4d.style.borderColor = "red";
+    btn4d.classList.remove('scale-up-center');
+} else if( i4d%2 == !0){
+    btn4d.style.background = "rgb(230, 204, 255)";
+    btn4d.style.color = "purple";
+    btn4d.style.borderColor = "rgb(145, 107, 242)";
+    btn4d.classList.add('scale-up-center');
+}
+});
+
+// Other group
+const football = document.getElementById('football');
+const footballOrange = football.style.background = "orange";
+let ifootball = 0;
+
+football.addEventListener("click", function onClick(){
+    ifootball++;
+    if ( ifootball%2 == 0){
+    football.style.background = "orange";
+    football.style.color = "white";
+    football.style.borderColor = "red";
+    football.classList.remove('scale-up-center');
+} else if( ifootball%2 == !0){
+    football.style.background = "rgb(230, 204, 255)";
+    football.style.color = "purple";
+    football.style.borderColor = "rgb(145, 107, 242)";
+    football.classList.add('scale-up-center');
+}
+});
+
+const chorus = document.getElementById('chorus');
+const chorusOrange = chorus.style.background = "orange";
+let ichorus = 0;
+
+chorus.addEventListener("click", function onClick(){
+    ichorus++;
+    if ( ichorus%2 == 0){
+    chorus.style.background = "orange";
+    chorus.style.color = "white";
+    chorus.style.borderColor = "red";
+    chorus.classList.remove('scale-up-center');
+} else if( ichorus%2 == !0){
+    chorus.style.background = "rgb(230, 204, 255)";
+    chorus.style.color = "purple";
+    chorus.style.borderColor = "rgb(145, 107, 242)";
+    chorus.classList.add('scale-up-center');
+}
+});
+
+const Orchestra = document.getElementById('Orchestra');
+const OrchestraOrange = Orchestra.style.background = "orange";
+let iOrchestra = 0;
+
+Orchestra.addEventListener("click", function onClick(){
+    iOrchestra++;
+    if ( iOrchestra%2 == 0){
+        Orchestra.style.background = "orange";
+        Orchestra.style.color = "white";
+        Orchestra.style.borderColor = "red";
+        Orchestra.classList.remove('scale-up-center');
+} else if( iOrchestra%2 == !0){
+    Orchestra.style.background = "rgb(230, 204, 255)";
+    Orchestra.style.borderColor = "rgb(145, 107, 242)";
+    Orchestra.classList.add('scale-up-center');
+}
+});
+
+const DIY = document.getElementById('DIY');
+const DIYOrange = DIY.style.background = "orange";
+let iDIY = 0;
+
+DIY.addEventListener("click", function onClick(){
+    iDIY++;
+    if ( iDIY%2 == 0){
+        DIY.style.background = "orange";
+        DIY.style.color = "white";
+        DIY.style.borderColor = "red";
+        DIY.classList.remove('scale-up-center');
+} else if( iDIY%2 == !0){
+    DIY.style.background = "rgb(230, 204, 255)";
+    DIY.style.borderColor = "rgb(145, 107, 242)";
+    DIY.classList.add('scale-up-center');
+}
+});
+
+const rainbow = document.getElementById('rainbow');
+const rainbowOrange = rainbow.style.background = "orange";
+let irainbow = 0;
+
+rainbow.addEventListener("click", function onClick(){
+    irainbow++;
+    if ( irainbow%2 == 0){
+        rainbow.style.background = "orange";
+        rainbow.style.color = "white";
+        rainbow.style.borderColor = "red";
+        rainbow.classList.remove('scale-up-center');
+} else if( irainbow%2 == !0){
+    rainbow.style.background = "rgb(230, 204, 255)";
+    rainbow.style.borderColor = "rgb(145, 107, 242)";
+    rainbow.classList.add('scale-up-center');
+}
+});
+
+const Farmer = document.getElementById('Farmer');
+const FarmerOrange = Farmer.style.background = "orange";
+let iFarmer = 0;
+
+Farmer.addEventListener("click", function onClick(){
+    iFarmer++;
+    if ( iFarmer%2 == 0){
+        Farmer.style.background = "orange";
+        Farmer.style.color = "white";
+        Farmer.style.borderColor = "red";
+        Farmer.classList.remove('scale-up-center');
+} else if( iFarmer%2 == !0){
+    Farmer.style.background = "rgb(230, 204, 255)";
+    Farmer.style.borderColor = "rgb(145, 107, 242)";
+    Farmer.classList.add('scale-up-center');
+}
+});
+
+const Drama = document.getElementById('Drama');
+const DramaOrange = Drama.style.background = "orange";
+let iDrama = 0;
+
+Drama.addEventListener("click", function onClick(){
+    iDrama++;
+    if ( iDrama%2 == 0){
+    Drama.style.background = "orange";
+    Drama.style.color = "white";
+    Drama.style.borderColor = "red";
+    Drama.classList.remove('scale-up-center');
+} else if( iDrama%2 == !0){
+    Drama.style.background = "rgb(230, 204, 255)";
+    Drama.style.borderColor = "rgb(145, 107, 242)";
+    Drama.classList.add('scale-up-center');
+}
+});
+
+const lala = document.getElementById('lala');
+const lalaOrange = lala.style.background = "orange";
+let ilala = 0;
+
+lala.addEventListener("click", function onClick(){
+    ilala++;
+    if ( ilala%2 == 0){
+    lala.style.background = "orange";
+    lala.style.color = "white";
+    lala.style.borderColor = "red";
+    lala.classList.remove('scale-up-center');
+} else if( ilala%2 == !0){
+    lala.style.background = "rgb(230, 204, 255)";
+    lala.style.borderColor = "rgb(145, 107, 242)";
+    lala.classList.add('scale-up-center');
+}
+});
+
+const flat = document.getElementById('flat');
+const flatOrange = flat.style.background = "orange";
+let iflat = 0;
+
+flat.addEventListener("click", function onClick(){
+    iflat++;
+    if ( iflat%2 == 0){
+        flat.style.background = "orange";
+        flat.style.color = "white";
+        flat.style.borderColor = "red";
+        flat.classList.remove('scale-up-center');
+} else if( iflat%2 == !0){
+    flat.style.background = "rgb(230, 204, 255)";
+    flat.style.borderColor = "rgb(145, 107, 242)";
+    flat.classList.add('scale-up-center');
+}
+});
+
+const HappyA = document.getElementById('HappyA');
+const HappyArange = HappyA.style.background = "orange";
+let iHappyA = 0;
+
+HappyA.addEventListener("click", function onClick(){
+    iHappyA++;
+    if ( iHappyA%2 == 0){
+        HappyA.style.background = "orange";
+        HappyA.style.color = "white";
+        HappyA.style.borderColor = "red";
+        HappyA.classList.remove('scale-up-center');
+} else if( iHappyA%2 == !0){
+    HappyA.style.background = "rgb(230, 204, 255)";
+    HappyA.style.borderColor = "rgb(145, 107, 242)";
+    HappyA.classList.add('scale-up-center');
+}
+});
+
+const WalkA = document.getElementById('WalkA');
+const WalkAOrange = WalkA.style.background = "orange";
+let iWalkA = 0;
+
+WalkA.addEventListener("click", function onClick(){
+    iWalkA++;
+    if ( iWalkA%2 == 0){
+        WalkA.style.background = "orange";
+        WalkA.style.color = "white";
+        WalkA.style.borderColor = "red";
+        WalkA.classList.remove('scale-up-center');
+} else if( iWalkA%2 == !0){
+    WalkA.style.background = "rgb(230, 204, 255)";
+    WalkA.style.borderColor = "rgb(145, 107, 242)";
+    WalkA.classList.add('scale-up-center');
+}
+});
+
+const lego = document.getElementById('lego');
+const legoOrange = lego.style.background = "orange";
+let ilego = 0;
+
+lego.addEventListener("click", function onClick(){
+    ilego++;
+    if ( ilego%2 == 0){
+        lego.style.background = "orange";
+        lego.style.color = "white";
+        lego.style.borderColor = "red";
+        lego.classList.remove('scale-up-center');
+} else if( ilego%2 == !0){
+    lego.style.background = "rgb(230, 204, 255)";
+    lego.style.borderColor = "rgb(145, 107, 242)";
+    lego.classList.add('scale-up-center');
+}
+});
